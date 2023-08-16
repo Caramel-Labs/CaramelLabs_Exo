a97b3d62d55d1d702a60d0c2d290c98f136356a8
Manujaya <73481420+Iam-SilentEyes@users.noreply.github.com>
2023-08-16 09:15:11 +0530

    gg

--- exo-frontend/src/app/components/atoms/blueButton.tsx
+++ exo-frontend/src/app/components/atoms/blueButton.tsx
@@ -1,10 +1,18 @@
+import Image from "next/image"
+
 type BlueButtonProps = {
   text: string
+  sgcLogo: boolean
 }
+
+const logoPath: string = ""
 
 export default function BlueButton(props: BlueButtonProps) {
   return (
     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      {props.sgcLogo && (
+        <Image src={logoPath} height={30} width={30} alt="sgc-logo" />
+      )}
       {props.text}
     </button>
   )
