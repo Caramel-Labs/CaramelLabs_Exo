52e271aa593b775f2bceb00056b631f0045b9fc6
Manujaya <73481420+Iam-SilentEyes@users.noreply.github.com>
2023-08-17 08:40:04 +0530

    Just changes

---
+++ exo-frontend/src/components/bookFlight/bookFlight.tsx
@@ -0,0 +1,10 @@
+import Description from "@/components/shared/description"
+
+export default function bookFlight() {
+  return (
+    <div>
+      <h1>One way</h1>
+      <h1>Round trip</h1>
+    </div>
+  )
+}
