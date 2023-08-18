d186c38f3d2da79daa83e24f6484660b90150496
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2023-08-18 23:11:08 +0530

    Merge error fixed

--- exo-frontend/src/app/booking/page.tsx
+++ exo-frontend/src/app/booking/page.tsx
@@ -1,10 +1,13 @@
 import BookFlight from "@/components/bookFlight/bookFlight"
 import SelectSeats from "@/components/selectSeats/selectSeats"
+import getTripData from "@/utils/getTripData"
+import { data } from "autoprefixer"
 
 export default async function Booking() {
   const data = await getTripData()
   return (
     <main className="min-h-screen px-4 bg-black">
+      <BookFlight props={data} />
       <SelectSeats />
     </main>
   )
