97b6b0a106f2c42729187374ca4e59576da7693d
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2023-08-18 22:56:34 +0530

    Minor change

--- exo-frontend/src/app/booking/page.tsx
+++ exo-frontend/src/app/booking/page.tsx
@@ -3,7 +3,7 @@ import BookFlight from "@/components/bookFlight/bookFlight"
 export default async function Booking() {
   const data = await getTripData()
   return (
-    <main className="px-4 bg-black">
+    <main className="min-h-screen px-4 bg-black">
       <BookFlight />
     </main>
   )
