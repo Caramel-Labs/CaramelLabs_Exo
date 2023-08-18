d44b1e9d2e0c7496edd633cbf8de89234c5973ea
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2023-08-18 22:47:36 +0530

    Basic structure added to "Select Seats"

--- exo-frontend/src/app/booking/page.tsx
+++ exo-frontend/src/app/booking/page.tsx
@@ -1,11 +1,13 @@
 "use-client"
 
 import BookFlight from "@/components/bookFlight/bookFlight"
+import SelectSeats from "@/components/selectSeats/selectSeats"
 
 export default function Booking() {
   return (
-    <main className="px-4 bg-black">
-      <BookFlight />
+    <main className="min-h-screen px-4 bg-black">
+      {/* change tag as necessary during development */}
+      <SelectSeats />
     </main>
   )
 }
