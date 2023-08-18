4f2ebfc50ce1510ef0e044a509ebf2bd25fdddd5
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2023-08-19 00:29:33 +0530

    Bug fix in travel class button

--- exo-frontend/src/components/bookFlight/travelClassButton.tsx
+++ exo-frontend/src/components/bookFlight/travelClassButton.tsx
@@ -10,7 +10,7 @@ type travelClassButtonProps = {
 export default function TravelClassButton(props: travelClassButtonProps) {
   return (
     <div className="text-white">
-      <button className="bg-blue-500 hover:bg-blue-700 flex flex-col rounded-lg w-24 h-24 justify-center" >
+      <button className="bg-blue-500 hover:bg-blue-700 flex flex-col rounded-lg w-24 h-24 justify-center items-center" >
         <p className="text-4xl font-bold">{props.icon}</p>
         <p className="text-sm font-bold">
           {props.name1}
