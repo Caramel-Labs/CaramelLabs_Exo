06073c93bc66b3dc82405807d5b978e5a1ac5ebb
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2023-08-18 23:08:09 +0530

    Update page.tsx

--- exo-frontend/src/app/booking/page.tsx
+++ exo-frontend/src/app/booking/page.tsx
@@ -1,10 +1,11 @@
 import BookFlight from "@/components/bookFlight/bookFlight"
+import SelectSeats from "@/components/selectSeats/selectSeats"
 
 export default async function Booking() {
   const data = await getTripData()
   return (
     <main className="min-h-screen px-4 bg-black">
-      <BookFlight />
+      <SelectSeats />
     </main>
   )
 }
