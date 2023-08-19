fee0c1b645c1de0eb32181d591adf9b43a298c1b
Manujaya <73481420+Iam-SilentEyes@users.noreply.github.com>
2023-08-19 11:15:10 +0530

    Minor changes

--- exo-frontend/src/app/booking/page.tsx
+++ exo-frontend/src/app/booking/page.tsx
@@ -1,11 +1,15 @@
 import BookFlight from "@/components/bookFlight/bookFlight"
+import Checkout from "@/components/checkout/checkout"
 import SelectSeats from "@/components/selectSeats/selectSeats"
+import getTripData from "@/utils/getTripData"
+import { data } from "autoprefixer"
+import ConfirmBooking from "@/components/confirmBooking/confirmBooking"
 
 export default async function Booking() {
   const data = await getTripData()
   return (
     <main className="min-h-screen px-4 bg-black">
-      <SelectSeats />
+      <ConfirmBooking />
     </main>
   )
 }
---
+++ exo-frontend/src/app/seats/page.tsx
@@ -0,0 +1,9 @@
+import SelectSeats from "@/components/selectSeats/selectSeats"
+
+export default function Seats() {
+    return (
+        <main className="min-h-screen px-4 bg-black">
+            <SelectSeats />
+        </main>
+    )
+}
