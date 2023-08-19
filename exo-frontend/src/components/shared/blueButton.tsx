f312517a4874e39e1c383827dffd2227c0019e08
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2023-08-19 22:55:55 +0530

    Added SGC logo to blue button

--- exo-frontend/src/components/shared/blueButton.tsx
+++ exo-frontend/src/components/shared/blueButton.tsx
@@ -6,18 +6,18 @@ type BlueButtonProps = {
   onClick?: () => void // @dev onClick function is optional
 }
 
-const logoPath: string = ""
+const logoPath: string = "/sgc-logo.png"
 
 export default function BlueButton(props: BlueButtonProps) {
   return (
     <button
-      className="bg-blue-500 hover:bg-blue-700 text-white text-xs font-bold py-2.5 px-14 rounded"
+      className="bg-blue-500 hover:bg-blue-700 text-white text-xs font-bold py-2.5 px-14 rounded flex justify-center"
       onClick={props.onClick}
     >
       {props.sgcLogo && (
-        <Image src={logoPath} height={30} width={30} alt="sgc-logo" />
+        <Image src={logoPath} height={20} width={27} alt="SGC Logo" className="mr-4" />
       )}
-      {props.text}
+      <p>{props.text}</p>
     </button>
   )
 }
