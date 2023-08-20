bc59c249dd855a45f471d70b7d6df4148204742c
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2023-08-20 10:24:24 +0530

    Added back button to header gg

--- exo-frontend/src/components/shared/pageHeader.tsx
+++ exo-frontend/src/components/shared/pageHeader.tsx
@@ -1,11 +1,17 @@
+import Image from "next/image"
+
 type headerProps = {
   title: string
 }
 
 export default function PageHeader(props: headerProps) {
   return (
-    <header className="page-header">
+    <header className="page-header flex justify-between items-center">
+      <button>
+        <Image src={'/back.png'} width={24} height={24} alt={'Back icon'} />
+      </button>
       <h1 className="text-xl text-center py-4">{props.title}</h1>
+      <Image src={'/menu.png'} width={24} height={24} alt={'Back icon'} />
     </header>
   )
 }
