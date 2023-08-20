a6abf0b632d6bbbd2ae1f936d86557d2e2ee83b4
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2023-08-20 13:53:08 +0530

    Bug fix in multi-step form

--- exo-frontend/src/app/booking/page.tsx
+++ exo-frontend/src/app/booking/page.tsx
@@ -1,12 +1,13 @@
 import getTripData from "@/utils/getTripData"
-import ActiveStepForm from "@/components/shared/ActiveStepFrom"
+// import ActiveStepForm from "@/components/shared/ActiveStepFrom"
+import BookingStepForm from "@/components/shared/BookingStepFrom"
 
 export default async function Booking() {
   const data = await getTripData()
   //console.log(data.arrival)
   return (
     <main className="min-h-screen px-4 bg-black">
-      <ActiveStepForm props={data} />
+      <BookingStepForm props={data} />
     </main>
   )
 }
