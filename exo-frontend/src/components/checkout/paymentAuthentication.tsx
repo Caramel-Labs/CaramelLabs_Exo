85bb4fb2916014b0f002da22ecebf0e1972fd28e
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2023-08-20 22:30:25 +0530

    Update paymentAuthentication.tsx

--- exo-frontend/src/components/checkout/paymentAuthentication.tsx
+++ exo-frontend/src/components/checkout/paymentAuthentication.tsx
@@ -2,9 +2,9 @@ import Image from "next/image"
 
 export default function PaymentAuthentication() {
   return (
-    <main className="flex flex-col justify-center items-center">
-      <h1 className="font-bold text-3xl">Authenticating Payment</h1>
-      <p className="text-xs">Exo is authenticating your payment with your StarGate NeuraChip™...</p>
+    <main className="flex flex-col">
+      <h1 className="">Authenticating Payment</h1>
+      <p className="">Exo is authenticating your payment with your StarGate NeuraChip™...</p>
       <Image src={'/loader.gif'} width={1280} height={720} alt="Loader" />
     </main>
   )
