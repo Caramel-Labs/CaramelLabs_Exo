fa09c56f26f8dae73958ddb790279388d0f7a5d1
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2023-08-20 13:51:05 +0530

    Added 'X's to card number being rendered on card

--- exo-frontend/src/components/checkout/bankCard.tsx
+++ exo-frontend/src/components/checkout/bankCard.tsx
@@ -3,14 +3,26 @@ type bankCardProps = {
   cardNumber: string
 }
 
-// Custom function to format number with commas and a space every 3 digits
+// Format bank balance with commas and a space every 3 digits
 const formatNumberWithCommas = (number: number) => {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ", ");
 };
 
+// Format card number
+const formatCardNumber = (cardNumber: string) => {
+    // Remove spaces from the card number
+    const trimmedCardNumber = cardNumber.replace(/\s/g, '');
+
+    // Insert "XXXX" for the middle 8 digits
+    const formattedCardNumber = trimmedCardNumber.replace(/^(\d{4})(\d{4})(\d{4})(\d{4})$/, '$1 XXXX XXXX $4');
+
+    return formattedCardNumber;
+};
+
 export default function BankCard(props: bankCardProps) {
-    // Format balance with commas
+
     const formattedBalance = formatNumberWithCommas(props.balance);
+    const formattedCardNumber = formatCardNumber(props.cardNumber);
 
     return (
         <main className="w-[19.5rem] mx-2 h-44 bg-gradient-to-b from-cyan-500 to-blue-500 rounded-lg p-4 flex flex-col justify-between">
@@ -23,7 +35,7 @@ export default function BankCard(props: bankCardProps) {
                     <p>SGC</p>
                 </div>
             </div>
-            <p className="flex justify-center items-center text-xl tracking-widest">{props.cardNumber}</p>
+            <p className="flex justify-center items-center text-xl tracking-widest">{formattedCardNumber}</p>
         </main>
     )
 }
